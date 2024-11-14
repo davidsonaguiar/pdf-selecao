7bcbef51ada3d3de7b021a1af754ae1161eb1890
Davidson <davidson.05@outlook.com>
2024-11-13 23:44:05 -0300

    estilizado sem flex

--- main.js
+++ main.js
@@ -9,27 +9,6 @@ const candidatos = [
         gee: "GEE123456",
         escola: "Escola Estadual ABC",
     },
-    // {
-    //     nome: "Maria Oliveira",
-    //     dataNascimento: "1998-07-20",
-    //     cpf: "987.654.321-00",
-    //     gee: "GEE654321",
-    //     escola: "Escola Municipal XYZ"
-    // },
-    // {
-    //     nome: "Carlos Pereira",
-    //     dataNascimento: "2001-11-10",
-    //     cpf: "456.123.789-00",
-    //     gee: "GEE789123",
-    //     escola: "Escola Técnica QRS"
-    // },
-    // {
-    //     nome: "Ana Costa",
-    //     dataNascimento: "1999-03-30",
-    //     cpf: "321.654.987-00",
-    //     gee: "GEE321654",
-    //     escola: "Escola de Ensino Fundamental LMP"
-    // }
 ];
 
 function gerarPDF(html, cpf) {
